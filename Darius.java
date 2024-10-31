11a77a0aa54d1e7950836adc9853127039cf0047
Fernando <ferfirct@outlook.com>
2024-10-31 09:24:28 -0300

    Classe 2

--- Darius.java
+++ Darius.java
@@ -6,7 +6,7 @@ public class Darius extends Campeao {
                 "Mana",
                 "Darius é o líder das forças de Noxus e um dos guerreiros mais temidos em Valoran. Ele é conhecido por sua força bruta e determinação inabalável.",
                 new String[] {"Dizimar", "Ataque Mutilador", "Guilhotina de Noxus", "Hemorragia"},
-                new Classe.Lutador() // Classe inicial como Lutador
+                new Classe.Lutador()
         );
     }
 
